b0c3301c022caec8690fb3196493d0da2db00b4c
刘峥嵘 <liuzhengrong@fastschool.cn>
2018-09-07 14:56:54 +0800

    提交

---
+++ runtimeMethods/runtimeMethods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtimeMethods
+//
+//  Created by 刘峥嵘 on 2018/9/7.
+//  Copyright © 2018 刘峥嵘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtimeMethods/runtimeMethods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtimeMethods
+//
+//  Created by 刘峥嵘 on 2018/9/7.
+//  Copyright © 2018 刘峥嵘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
